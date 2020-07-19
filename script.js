a0dda9532b309a5a164f1fce5d3314719b7297ac
Jeneth Diesta <jen6one9@gmail.com>
2020-07-18 17:13:54 -0700

    added console logs along the way

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var person =prompt("Select desired password length (8 min - 128 max characters)") //User must select a value between 8 and 128
-var person =prompt("Create your password with upper-case letters? (y/n)")
-var person =prompt("How about with lower-case letters? (y/n)")
-var person =prompt("What about some numeric values? (y/n)")
-var person =prompt("Lastly, shall we include symbols? (y/n)")
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-}
-  function getRandomLowercase(){
-  }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//User must select a value between 8 and 128
+var characters = prompt("Select desired password length   (8 min - 128 max characters)", "");
+console.log(characters);
+var upper = prompt("Generate your password with upper-case letters?   (y/n)")  // Creates a prompt box which takes in text input.
+console.log(upper);
+var lower = prompt("How about with some lower-case letters?   (y/n)")
+console.log(lower);
+var numeric = prompt("Would you like some numeric values?   (y/n)")
+console.log(numeric);
+var symbols = prompt("Lastly, shall we include symbols?   (y/n)")
+console.log(symbols);
